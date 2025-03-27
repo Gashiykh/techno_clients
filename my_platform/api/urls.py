55d6bf4401bc43d81d80bba3f15c31d9c9ca91d8
Кабидоллаев Гашиых <gashiykh@bk.ru>
2025-03-27 16:53:56 +0500

    Изменил путь

--- my_platform/api/urls.py
+++ my_platform/api/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from api import views
 
 urlpatterns = [
-    path("clients/add/", views.ClientCreateView.as_view(), name='clients_add'),
+    path("clients/", views.ClientCreateView.as_view(), name='clients_add'),
 ]
